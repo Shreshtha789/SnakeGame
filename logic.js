818b3ac554fbde74d769672a3327d5995a5937c2
Shreshtha789 <shreshthapahwa21@gmail.com>
2023-02-23 21:27:34 +0530

    snake start moving and eating food

--- logic.js
+++ logic.js
@@ -43,12 +43,12 @@ function gameEngine(){
 
     //If you have eaten the food, increment the score and regenerate the food.
 
-    if(snakeArr[0].y === food.y && snakeArr[0].x === food.y){
+    if(snakeArr[0].y === food.y && snakeArr[0].x === food.x){
         snakeArr.unshift({x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y});
         foodSound.play();
         let a =2;
         let b=16;
-        food = {x: Math.round(a+(b-a)*Math.round), y: Math.round(a+(b-a)*Math.round)};
+        food = {x: Math.round(a+(b-a)*Math.random()), y: Math.round(a+(b-a)*Math.random())};
     }
 
     //Moving the snake:
@@ -61,5 +61,62 @@ function gameEngine(){
     snakeArr[0].x += inputDir.x;
     snakeArr[0].y += inputDir.y;
 
-    
+    //Part: 2 display the snake and food:
+
+    //Display the snake:
+    let board = document.getElementById('board');
+    board.innerHTML = '';
+    snakeArr.forEach((elements, index)=>{
+        let snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = elements.y;
+        snakeElement.style.gridColumnStart = elements.x;
+        if(index===0){
+            snakeElement.classList = 'snake_head';
+        }
+        else{
+            snakeElement.classList = 'snake_body';
+        }
+        board.appendChild(snakeElement);
+    })
+
+    //Display the food:
+    let foodElement = document.createElement('div');
+    foodElement.style.gridRowStart = food.y;
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.classList = 'food';
+    board.appendChild(foodElement);
 }
+
+
+window.addEventListener('keydown', (e)=>{
+
+    moveSound.play();
+    switch(e.key){
+        case 'ArrowUp':
+            inputDir.x = 0;
+            inputDir.y = -1;
+            console.log('ArrowUp');
+            break;
+
+        case 'ArrowDown':
+            inputDir.x = 0;
+            inputDir.y = 1;
+            console.log('ArrowDown');
+            break;
+
+        case 'ArrowLeft':
+            inputDir.x = -1;
+            inputDir.y = 0;
+            console.log('ArrowLeft');
+            break;
+
+        case 'ArrowRight':
+            inputDir.x = 1;
+            inputDir.y = 0;
+            console.log('ArrowRight');
+            break;
+
+        default:
+            break;
+    }
+});
